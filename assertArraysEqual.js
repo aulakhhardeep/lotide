66b347da6449761589489b257143a30fb44d470f
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-02-04 14:04:42 -0700

    Compared two Arrays using new function

---
+++ assertArraysEqual.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if(arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (i = 0; i < arr2.length; i++) {
+      if(arr1[i] !== arr2[i]) {
+        return false;
+      } 
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+assertArraysEqual([1,2,3], [1,2,3]);
+assertArraysEqual([1,2], [1,3,3]);
+

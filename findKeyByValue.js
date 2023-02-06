95f49d089f69bcc91acd206725dad186667d222f
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-02-06 13:20:17 -0700

    funtion to find the value of key and compare to a given input, if key value matches with the given value, then return the key, if not return undefined

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (obj, val) {
+  for (const key of Object.keys(obj)) { // using of to itretion through key and Object.keys() to get keys of an object
+    //console.log(obj[key]);
+    if(obj[key] === val) { //obj[key] is giving the value of the key. 
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);

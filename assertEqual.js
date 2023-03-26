3f63b74bb5a37c4d5ec9927550d67a21a8d91833
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-03-25 18:50:04 -0600

    Added assertEqual file

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+//test code
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);

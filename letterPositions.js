7aae5a1fc31f448768068bf78a264743a98f341d
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-03-30 13:38:39 -0600

    Added function to find the position of letters in a sentence

---
+++ letterPositions.js
@@ -0,0 +1,42 @@
+const letterPositions = function(sentence) {
+  let sentenceWithoutSpace = sentence.replaceAll(" ", ""); // removing the space
+  const results = {}; // to store the final result
+  for (let j = 0; j < sentenceWithoutSpace.length; j++) {
+    {
+
+      if (results[sentenceWithoutSpace[j]]) {// here we are accessing a letter from an object.
+        results[sentenceWithoutSpace[j]] = [...results[sentenceWithoutSpace[j]],j];// .... operator is copying the current content of an array
+      }  else {
+        results[sentenceWithoutSpace[j]] = [j];
+      }
+    }
+  }
+  console.log(results);
+  return results;
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr2.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+//Test code
+assertArraysEqual(letterPositions("hello Hardeep").h, [0]); 

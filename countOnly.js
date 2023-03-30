f3eae67c6dad87e49258154a00c73170b9c18d2a
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-03-30 13:24:09 -0600

    Added comments

--- countOnly.js
+++ countOnly.js
@@ -4,7 +4,7 @@ const countOnly = function(allItems, itemsToCount) {
 const results = {};
 for(const item of allItems) {
   if(itemsToCount[item]) {
-  if (results[item]) {
+  if (results[item]) { // accessing item of an object.
     results[item] += 1;
   } else {
     results[item] = 1;
@@ -36,7 +36,7 @@ const firstNames = [
 ];
 //here firstNames is getting passed to allItems array.
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
+console.log(result1);
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);

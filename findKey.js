8c7a59c93173b4da643b2ba07650bd245df8f1b9
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-04-02 19:56:52 -0600

    Added findKey.js

---
+++ findKey.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  let keys = Object.keys(object);
+  //console.log(keys); // with this getting keys in a array.
+  for (let key of keys) {
+    //console.log(key); // getting keys individually.
+    if (callback(object[key])) {
+      //console.log(key);
+      return key;
+    } 
+  }
+  //console.log("undefined");// just to see if function is working
+  return undefined; 
+};
+
+object1 =  { 
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+
+assertEqual(findKey(object1, x => x.stars === 7), undefined); 
+assertEqual(findKey(object1, x => x.stars === 2,), "noma"); 
+assertEqual(findKey(object1, x => x.stars === 1,), "Blue Hill"); 

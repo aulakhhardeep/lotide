fc9b5f25c503a641fe144f9acc4637f24a5f2018
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-04-17 16:30:07 -0600

    removed console.log for output

--- tail.js
+++ tail.js
@@ -3,7 +3,6 @@ const tail = function(arr) {
   for (let i = 1; i < arr.length; i++) { //by starting the index from 1, removed the first element which was on index 0
     tailedArr.push(arr[i]);
   }
-  console.log(tailedArr);
   return tailedArr;
 };
 

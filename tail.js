db92cf01498a689c30341a52ba3b4de830821d9b
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-04-17 00:46:22 -0600

    Refractoring file

--- tail.js
+++ tail.js
@@ -1,20 +1,14 @@
 const tail = function(arr) {
-  return arr.slice(1); // slice is removing the first element of an array.
-};
-
-const assertEqual = function(actual, expected) {
-  if (actual.toString === expected.toString) { //toString is used to convert array into String
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  let tailedArr = [];
+  for (let i = 1; i < arr.length; i++) { //by starting the index from 1, removed the first element which was on index 0
+    tailedArr.push(arr[i]);
   }
+  console.log(tailedArr);
+  return tailedArr; 
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]); // this step will take the value of result through tail function, then tail fxn will remove the head element.
-assertEqual(result, ["Lighthouse", "Labs"]);
+module.exports = tail;
 
-//Test case: Check the original array
-/** const words = ["Yo Yo", "Labs"];
-tail(words);
-console.log(tail(words));
-assertEqual(words.length, 2;// here words.length **/
+
+
+

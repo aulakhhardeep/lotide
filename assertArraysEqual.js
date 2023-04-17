3bc9a21a60f95f9bed5a8ba8eebd3203ca9f5e31
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-04-17 01:11:18 -0600

    refractoring of file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,16 @@
-const eqArrays = function(arr1, arr2) {
-  if(arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (i = 0; i < arr2.length; i++) {
-      if(arr1[i] !== arr2[i]) {
-        return false;
-      } 
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = function(arr1, arr2) {
+const assertArraysEqual = (arr1, arr2) => {
   if (eqArrays(arr1, arr2)) {
     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
   } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
   }
 };
 
-assertArraysEqual([1,2,3], [1,2,3]);
-assertArraysEqual([1,2], [1,3,3]);
+module.exports = assertArraysEqual;
+
+ 
+
+
+

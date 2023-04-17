4bc6ca899f03544138a3c4b45fd6129dfe9086f7
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-04-17 12:01:28 -0600

    eslint the file

--- eqArrays.js
+++ eqArrays.js
@@ -2,12 +2,12 @@ const assertEqual = require('./assertEqual');
 const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) { // checking the length of both arrays.
     return false;
-  } 
+  }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
-      }
     }
+  }
 
   return true;
 };

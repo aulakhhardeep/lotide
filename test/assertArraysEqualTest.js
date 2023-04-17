0ae0d31e5fe854a03cfaaf28c70708182833a6a0
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-04-17 11:49:39 -0600

    Add test files

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -3,4 +3,4 @@ const assertArraysEqual = require('../assertArraysEqual');
 
 //Test Code
 assertArraysEqual([1,2,3], [1,2,3]); //output should be passed 1,2,3 === 1,2,3
-assertArraysEqual([1,2], [1,3,3]); //output should be passed 1,2 !== 1,3,3
+assertArraysEqual([5,7,3], [8,2,3]); 
---
+++ test/middleTest.js
@@ -0,0 +1,7 @@
+//test/middle
+const middle = require('../middle');
+const assertArraysEqual = require('../assertArraysEqual');
+
+//Test Code
+
+assertArraysEqual(middle([1,2,3,8,7,5,6]), [8]); 

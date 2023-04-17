88a397f39b2e9e92b778b6a016f742fb568fc738
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-04-17 11:59:50 -0600

    refactor the file

--- middle.js
+++ middle.js
@@ -1,11 +1,11 @@
 const assertArraysEqual = require('./assertArraysEqual');
 const middle = function(array) {
   let result = [];
-  if(array.length % 2 === 0) {
-      result.push(array[Math.floor(array.length / 2)]);
-    } else {
-      result.push(array[Math.round((array.length / 2) - 1)]);
-    }
+  if (array.length % 2 === 0) {
+    result.push(array[Math.floor(array.length / 2)]);
+  } else {
+    result.push(array[Math.round((array.length / 2) - 1)]);
+  }
   console.log(result);
   return result;
 };

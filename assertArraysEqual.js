96ac35de19a9ad5c2553c6635fd7f72f97368f14
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-04-19 18:41:59 -0600

    Add comments to describe functionality

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,8 @@
+//a function that prints an appropriate message after comparing the two arrays.
 const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2)) {
+const assertArraysEqual = (arr1, arr2) => { //passing two arrays as parameters
+  if (eqArrays(arr1, arr2)) { //using eqArrays function to compare the two arrays.
     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);

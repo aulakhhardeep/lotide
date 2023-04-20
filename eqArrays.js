80c9dc1037da4be962e76ef0060726a69236f424
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-04-19 18:44:32 -0600

    Add comments to describe functionality

--- eqArrays.js
+++ eqArrays.js
@@ -1,8 +1,9 @@
-const eqArrays = (arr1, arr2) => {
+//a function that takes in 2 arrays and returns true if the arrays are equal and false otherwise.
+const eqArrays = (arr1, arr2) => {//passing two arrays as parameters to function
   if (arr1.length !== arr2.length) { // checking the length of both arrays.
     return false;
   }
-  for (let i = 0; i < arr1.length; i++) {
+  for (let i = 0; i < arr1.length; i++) {//looping through each element of array
     if (arr1[i] !== arr2[i]) {
       return false;
     }

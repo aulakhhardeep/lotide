5a56d0e783ff57aa803306fe32824baf35af9196
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-04-19 19:47:16 -0600

    Add comments to describe functionality

--- map.js
+++ map.js
@@ -1,3 +1,4 @@
+//a function that takes in an array and a callback and returns a new array based on the results of the callback on each item in the array.
 const assertArraysEqual = require('./assertArraysEqual');
 
 const words = ["ground", "control", "to", "major", "tom"];

199d4bbe01a247a6f1a09dedc36863f6688c09a3
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-04-19 19:49:52 -0600

    Add comments to describe functionality

--- tail.js
+++ tail.js
@@ -1,3 +1,4 @@
+//a function that takes in an array and returns everything except the first element of the array.
 const tail = function(arr) {
   const tailedArr = [];
   for (let i = 1; i < arr.length; i++) {

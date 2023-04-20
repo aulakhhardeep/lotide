4b23e82c1585376bb500172377b788bcfe9ce64e
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-04-19 19:46:18 -0600

    Add comments to describe functionality

--- letterPositions.js
+++ letterPositions.js
@@ -1,20 +1,20 @@
+//a function that takes in a string and returns all indices of letter positions in the string.
 const assertArraysEqual = require('./assertArraysEqual');
 
 const letterPositions = function(sentence) {
   const results = {}; // to store the final result
   for (let j = 0; j < sentence.length; j++) {
     {
-     // if (sentence[j] !== ' ') {
-      if (results[sentence[j]]) {
-        results[sentence[j]] = [...results[sentence[j]],j];
-      }  else {
-        results[sentence[j]] = [j];
+      if (sentence[j] !== ' ') {
+        if (results[sentence[j]]) {
+          results[sentence[j]] = [...results[sentence[j]],j];
+        }  else {
+          results[sentence[j]] = [j];
+        }
       }
     }
-   // }
   }
-  delete results[' '];
-    return results;
+  return results;
 };
 
 

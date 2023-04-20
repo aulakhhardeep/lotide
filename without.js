72efb572560e8c2fc77f557ed9c677b8a1ce7a39
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-04-19 19:52:38 -0600

    Add comments to describe functionality

--- without.js
+++ without.js
@@ -1,3 +1,4 @@
+//a function that takes in a source array and an itemsToRemove arrays and returns only the items from the source array that are not in the itemsToRemove array.
 const eqArrays = function(arr1, arr2) {
   if(arr1.length !== arr2.length) {
     return false;
